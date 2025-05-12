b3dbaa956cf1c6d409d08ec2d14ff85d16e9135c
phiduymanh1 <manhpdph49571@gmail.com>
2025-05-12 21:35:16 +0700

    Tao services va controller ham load hoa don

---
+++ WebsiteTechWorldBackEnd/src/main/java/org/example/websitetechworld/Controller/AdminController/HoaDonAdminController/HoaDonAdminController.java
@@ -0,0 +1,24 @@
+package org.example.websitetechworld.Controller.AdminController.HoaDonAdminController;
+
+import org.example.websitetechworld.Dto.Response.AdminResponse.AdminResponseHoaDon.HoaDonAdminResponse;
+import org.example.websitetechworld.Services.AdminServices.HoaDonAdminServices.HoaDonAdminService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/hoa-don")
+public class HoaDonAdminController {
+    private final HoaDonAdminService hoaDonAdminService;
+
+    public HoaDonAdminController(HoaDonAdminService hoaDonAdminService) {
+        this.hoaDonAdminService = hoaDonAdminService;
+    }
+
+    @GetMapping
+    public List<HoaDonAdminResponse> getAll(){
+        return hoaDonAdminService.getAllHoaDon();
+    }
+}
