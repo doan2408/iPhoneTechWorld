6c4cd00ec44e1a6cefa12d7c2572159e8c33ffc0
phiduymanh1 <manhpdph49571@gmail.com>
2025-06-12 19:14:00 +0700

    fix(.): fix bug sau khi thay doi db

--- WebsiteTechWorldFrontEnd/src/views/Admin/HoaDon/HoaDonAdmin.vue
+++ WebsiteTechWorldFrontEnd/src/views/Admin/HoaDon/HoaDonAdmin.vue
@@ -168,6 +168,7 @@
                             <th class="table-th checkbox-column">
                                 <input type="checkbox" class="header-checkbox">
                             </th>
+                            <th class="table-th">STT</th>
                             <th class="table-th">Mã hóa đơn</th>
                             <th class="table-th">Người mua (Thời điểm giao dịch)</th>
                             <th class="table-th">Ngày tạo</th>
@@ -181,6 +182,9 @@
                         <tr v-for="(hoaDon, index) in hoaDons" :key="hoaDon.id" class="table-row">
                             <td class="table-td checkbox-column">
                                 <input type="checkbox" class="row-checkbox">
+                            </td>
+                            <td class="table-td">
+                                <div class="invoice-stt">{{ (pageNo * pageSize ) +index +1 }}</div>
                             </td>
                             <td class="table-td">
                                 <div class="invoice-code">{{ hoaDon.maHoaDon }}</div>
