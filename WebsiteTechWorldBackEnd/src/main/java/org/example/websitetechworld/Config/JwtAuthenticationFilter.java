9429e2b5a30915a364f4461e1c5c297da821e82a
doan-2408 <nguyenbadoan.qc2005@gmail.com>
2025-06-28 22:41:49 +0700

    merge mergeModel branch

--- WebsiteTechWorldBackEnd/src/main/java/org/example/websitetechworld/Config/JwtAuthenticationFilter.java
+++ WebsiteTechWorldBackEnd/src/main/java/org/example/websitetechworld/Config/JwtAuthenticationFilter.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JwtService ongjwtService;
     private final UserDetailsService userDetailsService;
     private final JwtService jwtService;
 
