702e2f2c3035af1c2136b6c7cb55411949f2abd3
doan-2408 <nguyenbadoan.qc2005@gmail.com>
2025-06-30 23:05:18 +0700

    update(TaiKhoan): update diaChi

--- WebsiteTechWorldBackEnd/src/main/java/org/example/websitetechworld/Controller/AdminController/TaiKhoanAdminController/ClientAdminController.java
+++ WebsiteTechWorldBackEnd/src/main/java/org/example/websitetechworld/Controller/AdminController/TaiKhoanAdminController/ClientAdminController.java
@@ -103,46 +103,14 @@ public class ClientAdminController {
         }
     }
 
-    //get dia chi from idKhachHang
+    //get all dia chi from idKhachHang
     @GetMapping("/addresses/{idKhachHang}")
     public ResponseEntity<?> getAllClientAddresses(@PathVariable int idKhachHang) {
         return ResponseEntity.ok(diaChiAdminService.getAllDiaChi(idKhachHang));
     }
 
-    //get DiaChi from idDiaChi
-    @GetMapping("/address/{idDiaChi}")
-    public ResponseEntity<?> getAllClientAddress(@PathVariable int idDiaChi) {
-        return ResponseEntity.ok(diaChiAdminService.getDiaChiById(idDiaChi));
-    }
 
-    //update diaChi of client man Admin
-    @PutMapping("/address/{idDiaChi}")
-    public ResponseEntity<?> updateDiaChi(@PathVariable int idDiaChi, @RequestBody @Valid AdminDiaChiRequest adminDiaChiRequest, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()) {
-            List<Map<String, String>> errors = bindingResult.getFieldErrors()
-                    .stream()
-                    .map(e -> Map.of("field", e.getField(),
-                            "message", e.getDefaultMessage()))
-                    .collect(Collectors.toList());
-            return ResponseEntity.badRequest().body(errors);
-        }
-        try {
-            return ResponseEntity.ok(diaChiAdminService.updateDiaChi(idDiaChi, adminDiaChiRequest));
-        }catch (ValidationException e) {
-            // Bắt riêng FieldException trả lỗi với field cụ thể
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrors());
-        }
-        catch (IllegalArgumentException e) {
-            // Trả về lỗi với field = "other" để frontend biết là lỗi chung
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                    List.of(Map.of("field", "other", "message", e.getMessage()))
-            );
-        }
-        catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-                    List.of(Map.of("field", "other", "message", "Lỗi hệ thống: " + e.getMessage()))
-            );
-        }
-    }
+
+
 
 }
