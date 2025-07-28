38805cc2123ab5dad1d989a34127ab39c1d6540e
doan-2408 <nguyenbadoan.qc2005@gmail.com>
2025-07-28 23:41:00 +0700

    fix trang chu guest

--- WebsiteTechWorldFrontEnd/src/views/Guest/Home.vue
+++ WebsiteTechWorldFrontEnd/src/views/Guest/Home.vue
@@ -181,9 +181,7 @@ onMounted(() => {
         <div class="price-filter">
           <div class="filter-header">
             <div class="filter-icon">
-              <el-icon>
-                <PriceTag />
-              </el-icon>
+              <el-icon><PriceTag /></el-icon>
             </div>
             <div class="filter-content">
               <span class="filter-label">Khoảng giá</span>
@@ -194,16 +192,30 @@ onMounted(() => {
             </div>
           </div>
           <div class="price-slider-container">
-            <el-slider v-model="priceRange" range :min="minPrice" :max="maxPrice" :step="priceStep"
-              :format-tooltip="formatPriceShort" class="price-slider" />
+            <el-slider
+              v-model="priceRange"
+              range
+              :min="minPrice"
+              :max="maxPrice"
+              :step="priceStep"
+              :format-tooltip="formatPriceShort"
+              class="price-slider"
+            />
           </div>
           <div class="quick-price-filters">
-            <el-button v-for="filter in quickPriceFilters" :key="filter.label" size="small" :type="
+            <el-button
+              v-for="filter in quickPriceFilters"
+              :key="filter.label"
+              size="small"
+              :type="
                 priceRange[0] === filter.range[0] &&
                 priceRange[1] === filter.range[1]
                   ? 'primary'
                   : 'default'
-              " @click="applyQuickPriceFilter(filter.range)" class="quick-filter-btn">
+              "
+              @click="applyQuickPriceFilter(filter.range)"
+              class="quick-filter-btn"
+            >
               {{ filter.label }}
             </el-button>
           </div>
@@ -213,26 +225,46 @@ onMounted(() => {
         <div class="chosen-filters" v-if="showClearFilter">
           <div class="chosen-filters-header">
             <span class="chosen-label">Bộ lọc đang áp dụng:</span>
-            <el-button type="danger" size="small" @click="clearFilters" class="clear-btn">
-              <el-icon>
-                <Delete />
-              </el-icon>
+            <el-button
+              type="danger"
+              size="small"
+              @click="clearFilters"
+              class="clear-btn"
+            >
+              <el-icon><Delete /></el-icon>
               Xóa tất cả
             </el-button>
           </div>
           <div class="chosen-tags">
-            <el-tag v-if="priceRange[0] !== minPrice || priceRange[1] !== maxPrice" closable
-              @close="priceRange = [minPrice, maxPrice]" type="info" class="filter-tag">
+            <el-tag
+              v-if="priceRange[0] !== minPrice || priceRange[1] !== maxPrice"
+              closable
+              @close="priceRange = [minPrice, maxPrice]"
+              type="info"
+              class="filter-tag"
+            >
               Giá: {{ formatPriceShort(priceRange[0]) }} -
               {{ formatPriceShort(priceRange[1]) }}
             </el-tag>
-            <el-tag v-if="selectedLoai" closable @close="selectedLoai = ''" type="info" class="filter-tag">
+            <el-tag
+              v-if="selectedLoai"
+              closable
+              @close="selectedLoai = ''"
+              type="info"
+              class="filter-tag"
+            >
               Loại:
               {{
-              loaiOptions.find((item) => item.value === selectedLoai)?.label
+                loaiOptions.find((item) => item.value === selectedLoai)?.label
               }}
             </el-tag>
-            <el-tag v-if="filterKeyword" closable @close="filterKeyword = ''" type="info" class="filter-tag">
+            <el-tag
+              v-if="filterKeyword"
+              closable
+              @close="filterKeyword = ''"
+              type="info"
+              class="filter-tag"
+            >
               Từ khóa: {{ filterKeyword }}
             </el-tag>
           </div>
@@ -243,25 +275,24 @@ onMounted(() => {
           <div class="filter-item">
             <el-dropdown trigger="click">
               <el-button class="filter-btn">
-                <el-icon>
-                  <Menu />
-                </el-icon>
+                <el-icon><Menu /></el-icon>
                 <span>
                   {{
-                  selectedLoai
-                  ? loaiOptions.find((item) => item.value === selectedLoai)
-                  ?.label
-                  : "Loại"
+                    selectedLoai
+                      ? loaiOptions.find((item) => item.value === selectedLoai)
+                          ?.label
+                      : "Loại"
                   }}
                 </span>
-                <el-icon class="el-icon--right">
-                  <ArrowDown />
-                </el-icon>
+                <el-icon class="el-icon--right"><ArrowDown /></el-icon>
               </el-button>
               <template #dropdown>
                 <el-dropdown-menu>
-                  <el-dropdown-item v-for="option in loaiOptions" :key="option.value"
-                    @click="selectedLoai = option.value">
+                  <el-dropdown-item
+                    v-for="option in loaiOptions"
+                    :key="option.value"
+                    @click="selectedLoai = option.value"
+                  >
                     {{ option.label }}
                   </el-dropdown-item>
                 </el-dropdown-menu>
@@ -271,35 +302,37 @@ onMounted(() => {
           <div class="filter-item">
             <el-dropdown trigger="click">
               <el-button class="filter-btn">
-                <el-icon>
-                  <Sort />
-                </el-icon>
+                <el-icon><Sort /></el-icon>
                 <span>{{ sortLabel }}</span>
-                <el-icon class="el-icon--right">
-                  <ArrowDown />
-                </el-icon>
+                <el-icon class="el-icon--right"><ArrowDown /></el-icon>
               </el-button>
               <template #dropdown>
                 <el-dropdown-menu>
-                  <el-dropdown-item @click="
+                  <el-dropdown-item
+                    @click="
                       selectedSort = '';
                       currentPage = 1;
                       fetchProducts();
-                    ">
+                    "
+                  >
                     Hủy sắp xếp
                   </el-dropdown-item>
-                  <el-dropdown-item @click="
+                  <el-dropdown-item
+                    @click="
                       selectedSort = 'giaAsc';
                       currentPage = 1;
                       fetchProducts();
-                    ">
+                    "
+                  >
                     Giá thấp đến cao
                   </el-dropdown-item>
-                  <el-dropdown-item @click="
+                  <el-dropdown-item
+                    @click="
                       selectedSort = 'giaDesc';
                       currentPage = 1;
                       fetchProducts();
-                    ">
+                    "
+                  >
                     Giá cao đến thấp
                   </el-dropdown-item>
                 </el-dropdown-menu>
@@ -314,11 +347,14 @@ onMounted(() => {
         <!-- Search bar -->
         <div class="search-section">
           <div class="search-wrapper">
-            <el-input v-model="filterKeyword" placeholder="Tìm kiếm sản phẩm..." class="search-input" clearable>
+            <el-input
+              v-model="filterKeyword"
+              placeholder="Tìm kiếm sản phẩm..."
+              class="search-input"
+              clearable
+            >
               <template #prefix>
-                <el-icon>
-                  <Search />
-                </el-icon>
+                <el-icon><Search /></el-icon>
               </template>
             </el-input>
           </div>
@@ -330,20 +366,38 @@ onMounted(() => {
         <!-- Products grid -->
         <div class="products-grid" v-show="hasProduct">
           <el-row :gutter="24" class="homeproduct">
-            <el-col v-for="sp in products" :key="sp.id" :xs="24" :sm="12" :md="8" :lg="6" :xl="6" class="product-col">
+            <el-col
+              v-for="sp in products"
+              :key="sp.id"
+              :xs="24"
+              :sm="12"
+              :md="8"
+              :lg="6"
+              :xl="6"
+              class="product-col"
+            >
               <router-link :to="`detail/${sp.id}`" class="product-link">
                 <div class="product-card">
                   <div class="product-image-container">
-                    <img :src="sp.hinhAnh || '/img/no-image.png'" alt="product" class="product-image" />
+                    <img
+                      :src="sp.hinhAnh || '/img/no-image.png'"
+                      alt="product"
+                      class="product-image"
+                    />
                     <div class="product-overlay">
                       <div class="quick-view-btn">
-                        <el-icon>
-                          <View />
-                        </el-icon>
+                        <el-icon><View /></el-icon>
                       </div>
                     </div>
                   </div>
-                  
+                  <div class="product-info">
+                    <h3 class="product-name">
+                      {{ sp.tenSanPham }} - {{ sp.maXuatXu }}
+                    </h3>
+                    <div class="product-price">
+                      {{ formatPrice(sp.giaBan) }}
+                    </div>
+                  </div>
                 </div>
               </router-link>
             </el-col>
@@ -351,9 +405,18 @@ onMounted(() => {
         </div>
 
         <!-- Pagination -->
-        <div class="pagination-container" v-show="hasProduct && totalProducts > pageSize">
-          <el-pagination v-model:current-page="currentPage" :page-size="pageSize" :total="totalProducts"
-            layout="prev, pager, next, jumper" :background="true" class="custom-pagination" />
+        <div
+          class="pagination-container"
+          v-show="hasProduct && totalProducts > pageSize"
+        >
+          <el-pagination
+            v-model:current-page="currentPage"
+            :page-size="pageSize"
+            :total="totalProducts"
+            layout="prev, pager, next, jumper"
+            :background="true"
+            class="custom-pagination"
+          />
         </div>
 
         <!-- Không có sản phẩm -->
@@ -547,8 +610,8 @@ onMounted(() => {
 }
 
 .search-input {
-  border-radius: 15px;
-  border: 1px solid #1172d4;
+  border-radius: 8px;
+  border: 1px solid #ced4da;
   padding: 10px 14px;
   font-size: 14px;
 }
@@ -774,12 +837,5 @@ onMounted(() => {
   .search-wrapper {
     margin-bottom: 8px;
   }
-}
-.compare-button {
-  margin-top: 8px;
-  font-size: 14px;
-  color: #1a73e8;
-  display: flex;
-  align-items: center;
 }
 </style>
