a009022abe0bb3dbb64ad5abb8feb864e954514c
doan2408 <nguyenbadoan2k5@gmail.com>
2025-08-26 06:07:15 +0700

    fix voucher, khuyen mai

--- WebsiteTechWorldFrontEnd/src/views/Admin/HoaDon/BanHangAdmin.vue
+++ WebsiteTechWorldFrontEnd/src/views/Admin/HoaDon/BanHangAdmin.vue
@@ -821,7 +821,9 @@ const calculateTotal = () => {
 
     if (selectedDiscount.value?.loaiGiamGia === 'Phần trăm') {
         discountAmount.value = totalProductAmount.value * selectedDiscount.value.giaTriGiamGia / 100;
+        console.log('jj', selectedDiscount.value)
         if (selectedDiscount.value?.giaTriGiamGiaToiDa < discountAmount.value) {
+            
             discountAmount.value = selectedDiscount.value?.giaTriGiamGiaToiDa
         }
     } else if (selectedDiscount.value?.giaTriGiamGia) {
@@ -2504,6 +2506,7 @@ const closeVoucherModal = () => {
 };
 
 const applyDiscount = (discount) => {
+    console.log('ll', discount)
     selectedDiscount.value = discount;
     isVoucherModalOpen.value = false;
 };
